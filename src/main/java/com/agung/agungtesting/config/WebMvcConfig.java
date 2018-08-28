2c9bf48b80d01691684e70bb0b57ed2b806de79b
agungputrap <real.stereo2@gmail.com>
2018-08-28 18:56:31 +0700

    fixing mas fahri

--- src/main/java/com/agung/agungtesting/config/WebMvcConfig.java
+++ src/main/java/com/agung/agungtesting/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -21,5 +22,15 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+//                .addResourceHandler("/static/**")
+//                .addResourceLocations("classpath:/public/static/");
+
+          .addResourceHandler("/**")
+                .addResourceLocations("classpath:/public/");
     }
 }
